c7d8e8ea9d6ba4cf9188c59dfd37009d654bef21
Omar <omarmeciry21@gmail.com>
2022-03-07 00:01:19 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // I/O
+//	     System.out.println("Enter your name: ");
+//
+//        Scanner s = new Scanner(System.in);
+//        var name = s.nextInt();
+//        System.out.println("Hello "+name);
+
+        // Data Types
+//        int i =- 10;
+//        double d= 10.5;
+//        char c = 'a';
+//        String str = "Omar";
+//        boolean b =false;
+
+        //Constant
+//        final double PIE = 3.14;
+
+        //Operators
+        // Arithmetic Operator
+//        double x = 10;
+//        double y = 20;
+//
+//        double z = x/y;
+//        System.out.println(z);
+
+        //Rational Operator
+//        boolean b = x==y;
+//        System.out.println(b);
+
+        //Logical Operators
+//        double gpa = 3.54;
+//
+//        boolean isA =gpa>3.7;
+//        boolean isB =gpa>3.3&&gpa<3.7;
+//    System.out.println("Is Grade equal A: "+isA);
+//    System.out.println("Is Grade equal B: "+isB);
+
+        //Unary Operator
+//        int i = -10;
+//       int n = ++i;
+//        System.out.println(i);
+//
+//        System.out.println(n);
+
+
+
+    }
+}
